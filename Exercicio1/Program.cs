778d4b28dca1e273f62d18ff129ca76ba9ce65a1
Joao Nacur <luisoujpof@gmail.com>
2025-05-28 15:07:18 -0300

    Exercicio1

---
+++ Exercicio1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Definindo a classe Retangulo
+            Retangulo retangulo = new Retangulo();
+
+
+            // Solicitando ao usuário a largura do retângulo
+            Console.Write("Digite a largura do retângulo: ");
+            retangulo.largura = double.Parse(Console.ReadLine() , CultureInfo.InvariantCulture);
+
+            // Solicitando ao usuário a altura do retângulo
+            Console.Write("Digite a altura do retângulo: ");
+            retangulo.altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            // Mensagem de sucesso
+            Console.WriteLine("Retângulo criado com sucesso!");
+
+            // Calculando a área do retângulo
+            double area = retangulo.CalcularArea();
+            Console.WriteLine($"Área: {area.ToString("F2", CultureInfo.InvariantCulture)}");
+
+            // Calculando o perímetro do retângulo
+            double perimetro = retangulo.CalcularPerimetro();
+            Console.WriteLine($"Perímetro: {perimetro.ToString("F2", CultureInfo.InvariantCulture)}");
+
+            // Calculando a diagonal do retângulo
+            double diagonal = retangulo.CalcularDiagonal();
+            Console.WriteLine($"Diagonal: {diagonal.ToString("F2", CultureInfo.InvariantCulture)}");
+
+            // Mensagem de encerramento
+            Console.WriteLine("Pressione qualquer tecla para sair...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Exercicio1/Retangulo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Formats.Asn1;
+
+namespace Exercicio1
+{
+    internal class Retangulo
+    {
+        public double largura;
+        public double altura;
+
+        public double CalcularArea()
+        {
+            return largura * altura;
+        }
+
+        public double CalcularPerimetro()
+        {
+            return 2 * (largura + altura);
+        }
+
+        public double CalcularDiagonal()
+        {
+            return Math.Sqrt(largura * largura + altura * altura);
+        }
+    }
+}
