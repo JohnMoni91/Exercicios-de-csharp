b0ee330347e74fff25fb954b4c3e6bd563b6e1f1
Joao Nacur <luisoujpof@gmail.com>
2025-05-28 15:26:25 -0300

    ex2

---
+++ ex2/Exercicio2/Funcionario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace Exercicio2
+{
+    internal class Funcionario
+    {
+        public string nome;
+        public double salarioBruto;
+        public double imposto;
+
+        public double SalarioLiquido()
+        {
+            return salarioBruto - imposto;
+        }
+
+        public void AumentarSalario(double porcentagem)
+        {
+            salarioBruto += salarioBruto * (porcentagem / 100);
+        }
+
+        public override string ToString()
+        {
+            return $"{nome}, $ {SalarioLiquido():F2}";
+        }
+
+    }
+}
---
+++ ex2/Exercicio2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Exercicio2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        // Criação de um objeto Funcionario
+        Funcionario funcionario = new Funcionario();
+
+        // Leitura dos dados do funcionário
+
+        // Nome
+        Console.Write("Nome: ");
+        funcionario.nome = Console.ReadLine();
+
+        // Salário bruto
+        Console.Write("Salário bruto: ");
+        funcionario.salarioBruto = double.Parse(Console.ReadLine());
+
+        // Imposto
+        Console.Write("Imposto: ");
+        funcionario.imposto = double.Parse(Console.ReadLine());
+
+        // Exibir funcinario
+        Console.WriteLine($"Funcionário: {funcionario}");
+
+        // Aumento de salário
+        Console.Write("Digite a porcentagem para aumentar o salário: ");
+        double porcentagem = double.Parse(Console.ReadLine());
+        funcionario.AumentarSalario(porcentagem);
+
+        // Exibir funcinario após aumento
+        Console.WriteLine($"Dados atualizados: {funcionario}");
+
+
+        // Manter o console aberto
+        Console.WriteLine("-------------------------------------------------------");
+        Console.WriteLine("Pressione qualquer tecla para sair...");
+        Console.ReadKey();
+    }
+}
