d9af9ba5c73a5db1d261d83737c61b679344a098
Joao Nacur <luisoujpof@gmail.com>
2025-05-28 16:03:54 -0300

    Ex3

---
+++ Exercicio3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // declarando oa classe
+            SistemaEscolar sistema = new SistemaEscolar();
+
+            // solicitando os dados do aluno 
+            Console.Write("Digite o nome do aluno: ");
+            sistema.aluno = Console.ReadLine();
+
+            // Nota do aluno
+
+            // nota 1
+            Console.Write("Digite a nota 1: ");
+            sistema.nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            // nota 2
+            Console.Write("Digite a nota 2: ");
+            sistema.nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            // nota 3
+            Console.Write("Digite a nota 3: ");
+            sistema.nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            // calculando a média e a situação do aluno
+            double notaFinal = sistema.notaFinal();
+
+            // Exibindo os resultados formatados e se caso o aluno foi reprovado, quantos pontos faltam
+            if (sistema.SituacaoAluno())
+            {
+                Console.WriteLine($"A média final do aluno {sistema.aluno} é: {notaFinal.ToString("F2", CultureInfo.InvariantCulture)} pontos.");
+            }
+            else
+            {
+                double pontosFaltando = 60 - notaFinal;
+                Console.WriteLine($"O aluno {sistema.aluno} precisa de {pontosFaltando.ToString("F2", CultureInfo.InvariantCulture)} pontos para ser aprovado.");
+            }
+        }
+    }
+}
---
+++ Exercicio3/SistemaEscolar.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Exercicio3
+{
+    internal class SistemaEscolar
+    {
+        public string aluno;
+        public double nota1;
+        public double nota2;
+        public double nota3;
+
+        public double notaFinal()
+        {
+            return nota1 + nota2 + nota3;
+        }
+
+        // fazer a situação do aluno se ele reprovou, está de recuperação ou foi aprovado
+        // tbm colocar quantos pontos faltam
+
+        public bool SituacaoAluno()
+        {
+            if (notaFinal() < 60)
+            {
+                return false; // O aluno foi reprovado
+            }
+            else if (notaFinal() >= 60 && notaFinal() < 70)
+            {
+                return true; // O aluno está de recuperação
+            }
+            else
+            {
+                return true; // O aluno foi aprovado
+            }
+        }
+    }
+}
